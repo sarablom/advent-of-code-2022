d2d0bf3a2a2a0a71cd968b2d53f6ae4e4952fb60
Sara Blom <sara.cecilia.blom@gmail.com>
2022-12-04 10:13:46 +0100

    add second star of first day

--- 01-dec/index.ts
+++ 01-dec/index.ts
@@ -4,16 +4,27 @@ const data = fs.readFileSync("elf-calories.txt", "utf8").split("\n");
 // To run
 // npx ts-node index.ts
 let singleElfCount = 0;
-let inputSums = [0];
+let totalCaloriesSums = [0];
 
 for (let i = 0; i < data.length; i++) {
     if (data[i] === "") {
         singleElfCount++;
-        inputSums[singleElfCount] = 0;
+        totalCaloriesSums[singleElfCount] = 0;
         continue;
     }
 
-    inputSums[singleElfCount] += (+data[i]);
+    totalCaloriesSums[singleElfCount] += +data[i];
 }
 
-console.log(Math.max(...inputSums));
+console.log(Math.max(...totalCaloriesSums));
+
+const length = totalCaloriesSums.length;
+
+const sumOfThreeHighestCalorieCounts = totalCaloriesSums
+    .sort((a, b) => {
+        return a - b;
+    })
+    .slice(length - 3, length)
+    .reduce((acc, curr) => acc + curr, 0);
+
+console.log(sumOfThreeHighestCalorieCounts);
